2066c7992e9db8a9731d4c9f80880694508612d9
Alan King <alanking@Alan-Kings-MacBook.local>
2016-04-23 14:26:26 +0100

    add delete track meetings

--- angularregistration/js/controllers/meetings.js
+++ angularregistration/js/controllers/meetings.js
@@ -1,4 +1,38 @@
-myApp.controller('MeetingsController', ['$scope', function($scope){
+myApp.controller('MeetingsController', ['$scope', '$rootScope', '$firebaseAuth', '$firebaseArray', 'FIREBASE_URL', function($scope, $rootScope, $firebaseAuth, $firebaseArray, FIREBASE_URL){
     
+    var ref = new Firebase(FIREBASE_URL);
+    var auth = $firebaseAuth(ref);
+    
+    auth.$onAuth(function(authUser){
+        if(authUser){
+        var meetingsRef = new Firebase(FIREBASE_URL + 'users/' + $rootScope.currentUser.$id + '/meetings');
+            
+        var meetingInfo = $firebaseArray(meetingsRef);
+        $scope.meetings = meetingInfo;
+            
+        meetingInfo.$loaded().then(function(data){
+           $rootScope.howManyMeetings = meetingInfo.length;
+        }); // make sure meeting data is loaded
+            
+        meetingInfo.$watch(function(data){
+           $rootScope.howManyMeetings = meetingInfo.length;
+        }); // make sure meeting data is loaded
+            
+        $scope.addMeeting = function(){
+            meetingInfo.$add({
+                name: $scope.meetingname,
+                date: Firebase.ServerValue.TIMESTAMP
+            }).then(function(){
+                $scope.meetingname='';
+            }); //promise
+        }; // addMeeting
+            
+            
+        $scope.deleteMeeting = function(key){
+            meetingInfo.$remove(key);
+        }
+        
+        }// User Authenticated
+    });// on Auth
 
-}]);
+}]);// Controller
